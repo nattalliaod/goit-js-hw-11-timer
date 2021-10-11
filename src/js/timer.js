02e0cc162a8b17b11d54de73e08a23c089a3be04
Natalia Odinets <natashaodinec57@gmail.com>
2021-10-11 18:00:57 +0300

    Добавляет скрипты

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,55 +1,50 @@
 import refs from './refs'
 
-const { daysC, hoursC, minsC, secondsC } = refs
-
-class Timer {
-  constructor({ targetDate, markup }) {
-    this.refs = { daysC, hoursC, minsC, secondsC }
-    this.targetDate = targetDate;
-    this.deltaTime = 0;
-    this.markup = markup;
+class CountdownTimer {
+    constructor({ targetDate, selector }) {
+        const { daysC, hoursC, minsC, secondsC } = refs
+        this.selector = selector;
+        this.refs = { daysC, hoursC, minsC, secondsC }
+        this.targetDate = targetDate;
     
-  }
+        this.start()
+    }
 
-   start() {
-   setInterval(() => {
-     const currentTime = Date.now()
-    //  console.log(currentTime);
-     const deltaTime = this.targetDate - currentTime
-    //  console.log(this.targetDate);
-      const timeComponents = this.getTimeComponents(deltaTime)
+    start() {
+    setInterval(() => {
+      const deltaTime = this.targetDate - Date.now()
+        // const timeComponents = this.getTimeComponents(deltaTime)
         // console.log(timeComponents);
-        this.insertValues(timeComponents)
+        this.insertValues(this.getTimeComponents(deltaTime))
     }, 1000);
   }
 
-   getTimeComponents(time) {
-    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)))
-    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const seconds = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    getTimeComponents(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const seconds = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-      return { days,hours, mins, seconds };
+       return { days,hours, mins, seconds };
   }
 
-   pad(value) {
-    return String(value).padStart(2, '0');
+    pad(value) {
+      return String(value).padStart(2, '0');
   }
 
-   insertValues({ days, hours, mins, seconds }) {
-    this.refs.daysC.innerHTML = days;
-    this.refs.hoursC.innerHTML = hours;
-    this.refs.minsC.innerHTML = mins;
-    this.refs.secondsC.innerHTML = seconds;
+    insertValues({ days, hours, mins, seconds }) {
+      this.refs.daysC.innerHTML = days;
+      this.refs.hoursC.innerHTML = hours;
+      this.refs.minsC.innerHTML = mins;
+      this.refs.secondsC.innerHTML = seconds;
 }
 }
 
-const timer = new Timer({
+const timer = new CountdownTimer({
 
-  targetDate: new Date(`jan 1, 2022`),
- 
+    selector: '#timer-1',
+    targetDate: new Date('Jan 1, 2022'),
 });
-// Timer.start()
-timer.start()
+
 
 
